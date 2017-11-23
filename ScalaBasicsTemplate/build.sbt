83d3f902d5bf904d6d9131b357e34af2cfe2e651
Alexandra DeWit <alexa.dewit@evolvetechnology.se>
2017-11-23 16:34:04 +0100

    Working on writing some examples

--- ScalaBasicsTemplate/build.sbt
+++ ScalaBasicsTemplate/build.sbt
@@ -2,6 +2,9 @@ organization := "se.evolvetechnology"
 name := "Functional Programming in Scala Workshop"
 version := "0.0.1-SNAPSHOT"
 scalaVersion := "2.12.3"
+
+mainClass in (Compile, run) := Some("se.evolvetechnology.scala.workshop.basic.Main")
+
 scalacOptions ++= Seq(
   "-feature",
   "-language:higherKinds",
@@ -9,6 +12,11 @@ scalacOptions ++= Seq(
   "-Ypartial-unification"
 )
 assemblyJarName in assembly := "ScalaWorkshop.jar"
+
+libraryDependencies ++= Seq(
+  "org.typelevel" %% "cats-core"   % "1.0.0-RC1",
+  "org.typelevel" %% "cats-effect" % "0.5"
+)
 
 libraryDependencies ++= Seq(
   "org.scalatest"      %% "scalatest"           % "3.0.4"         % "test"
--- ScalaBasicsTemplate/src/main/scala/se/evolvetechnology/scala/workshop/basic/Main.scala
+++ ScalaBasicsTemplate/src/main/scala/se/evolvetechnology/scala/workshop/basic/Main.scala
@@ -1,5 +1,38 @@
 package se.evolvetechnology.scala.workshop.basic
 
-object Main extends App {
+import cats.effect.{IO}
+import scala.io.StdIn
+
+
+object Main {
+
+  def main(args: Array[String]): Unit = {
+    var canChange = 5 //a mutable Int... In the scala world we generally avoid var at all costs.
+    val betterConst = 5 //an immutable constant Int, but possible to determine at runtime!
+    Console.print("Synchronous Response:")
+    val runtimeConst = StdIn.readLine() //Notice here, val is created at runtime, yet remains immutable
+    //That said, we don't WANT to just run readLine() synchronously. Readline is a non-pure function! I don't like that stuff!
+    Console.println(runtimeConst)
+    //Here, lets model the act of retrieving a line of text from the console as a value in and of itself.
+    val safeRuntimeConst: IO[String] = IO(StdIn.readLine("\nIO Response:"))
+
+    //Now, run the application... Notice that we only ever get one input from the console. WHAT?
+    //The reason for this is that IO values are lazily evaluated. They simply encapsulate a definition for behaviour with types,
+    //but they do not themselves -execute- unless explicitly instructed to.
+    //Lets go ahead now then and -descript- the act of echoing that line back to the console, shall we?
+    val echoToConsole: IO[Unit] = safeRuntimeConst.map(input => Console.println(input))
+
+    //We'll find that, still, we get no action on the console. Maybe this is why people always say it's hard to "do anything"
+    //in haskell.... so far we're "describing" the doing all day, hell, we can map actions to other actions, compose them all day.
+
+
+    //Uncomment the line below, lets make it run the unsafe way, shall we?
+   // echoToConsole.unsafeRunSync() //Synchronously and unsafely run an IO.
+
+    //Wait a minute, it still didn't do anything?
+
+    //Of course not, it's running asynchronously in its own thread pool!
+  }
+
 
 }
